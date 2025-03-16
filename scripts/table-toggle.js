bcacdb6c8154b4f77dc6f14cf1bd4df0e33aafa1
LuxJakob <lux.jakob@gmail.com>
2025-03-16 17:50:14 +0100

    complete rework, finished resume

--- scripts/table-toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleTable(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section.style.display === "none") {
-        section.style.display = "table-row-group";
-    } else {
-        section.style.display = "none";
-    }
-}
